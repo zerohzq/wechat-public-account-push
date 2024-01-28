59d8365d0f81aa1b413af489e2665401d18323ec
zerohzq <97683244+zerohzq@users.noreply.github.com>
2024-01-28 18:50:41 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -1,4 +1,4 @@
-	vg3v_zXVUA801oOVSBq-iym6Ims2VNbmLGpH7m_r764/* eslint-disable */
+	vg3v_zXVUA801oOVSBq-iym6Ims2VNbmLGpH7m_r764	vg3v_zXVUA801oOVSBq-iym6Ims2VNbmLGpH7m_r764/* eslint-disable */
 
 /**
  * 此项目配置为方便新人使用，已缩减至最简配置。
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'c71740467c8ed0373088842dbbf9cfb2',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'vg3v_zXVUA801oOVSBq-iym6Ims2VNbmLGpH7m_r764',
+      useTemplateId: '	vg3v_zXVUA801oOVSBq-iym6Ims2VNbmLGpH7m_r764',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '09-08',
       festivals: [
