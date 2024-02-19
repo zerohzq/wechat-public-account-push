3fea4a0405c1b5802a86f9615c62ffaf347398e3
zerohzq <97683244+zerohzq@users.noreply.github.com>
2024-02-19 11:08:53 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oca6d6L8Wy8Hzds7qnIVAl5VT_L0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'pePTRv2qxT8e5x5WmucB-KP1eC3WKwQDzTPTkYnA3aI',
+      useTemplateId: 'HqjLomk2asmZqFJ1uhAK2L57oxhLsuhB3vJMmRdDDiA',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '09-08',
       festivals: [
