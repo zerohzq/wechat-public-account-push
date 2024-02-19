148b03dff607537f51fa8536611801c246dd0459
zerohzq <97683244+zerohzq@users.noreply.github.com>
2024-02-19 10:53:44 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -31,7 +31,7 @@ const USER_CONFIG = {
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '自己', year: '1998', date: '09-08',
         },
         {
           type: '节日', name: '相识纪念日', year: '2024', date: '02-16',
