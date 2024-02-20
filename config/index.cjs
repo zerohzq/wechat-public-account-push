3f4ca464e8df8539f9cfcb793c815a5a25c78b22
zerohzq <97683244+zerohzq@users.noreply.github.com>
2024-02-20 14:03:12 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -31,7 +31,7 @@ const USER_CONFIG = {
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '自己', year: '1998', date: '09-08',
+          type: '生日', name: '', year: '1998', date: '09-08',
         },
         {
           type: '节日', name: '相识纪念日', year: '2024', date: '02-16',
@@ -44,7 +44,7 @@ const USER_CONFIG = {
         // 结婚纪念日
         { keyword: 'marry_day', date: '2024-09-09' },
         
-        { keyword: 'birthdays', date: '2002-01-09' },
+        { keyword: 'wx_birthday_0', date: '2002-01-09' },
       ],
     },
   ],
