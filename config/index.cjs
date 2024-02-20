34ab7295aa24e61c2fa3fab88edd945c192dad77
zerohzq <97683244+zerohzq@users.noreply.github.com>
2024-02-20 14:56:19 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -17,9 +17,9 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: 'zero',
+      name: '来杯四季奶青',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oca6d6L8Wy8Hzds7qnIVAl5VT_L0',
+      id: 'oca6d6EV4mIEyeFSH4yXpMHqrlNg',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'M6zx9rEc1wXynqDkUZLe1VFVmBYC5nXxnlP7AS-7cSQ',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
