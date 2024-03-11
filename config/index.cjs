3b7246f7b03e6d5248fe0cbb8f1d5e6e6fc2e43b
zerohzq <97683244+zerohzq@users.noreply.github.com>
2024-03-11 13:39:01 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -27,15 +27,15 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '巧儿', year: '2002', date: '01-24',
+          type: '*生日', name: '宝贝', year: '2002', date: '01-24',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '生日', name: '巧儿', year: '2002', date: '01-08',
-        },
-        {
-          type: '节日', name: '相识纪念日', year: '2024', date: '02-16',
-        },
+      //  {
+        //  type: '生日', name: '巧儿', year: '2002', date: '01-08',
+      //  },
+       // {
+        //  type: '节日', name: '相识纪念日', year: '2024', date: '02-16',
+        //},
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
