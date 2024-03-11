058cdf600fa1cc0c37277a172c8d4231ef4f037e
zerohzq <97683244+zerohzq@users.noreply.github.com>
2024-03-11 13:50:10 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,7 +19,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oca6d6L8Wy8Hzds7qnIVAl5VT_L0',
+      id: 'oca6d6EV4mIEyeFSH4yXpMHqrlNg',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'Ha1gRJpciC-oYWxmGjX-iS1mqwCrQ2wWw7pvF_1CQSc',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
